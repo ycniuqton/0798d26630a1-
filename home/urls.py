680bcfad2dca190707de332222b99609fdfe76db
Quinh <quinh>
2024-07-10 00:53:38 +0700

    add template

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,26 @@ from . import views
 urlpatterns = [
   path(''       , views.index,  name='index'),
   path('tables/', views.tables, name='tables'),
+
+
+  path('instances/', views.instances, name='instances'),
+  path('create_instances/', views.create_instances, name='create_instances'),
+
+  path('network/', views.network, name='network'),
+  path('block_storage/', views.block_storage, name='block_storage'),
+  path('snapshot/', views.snapshot, name='snapshot'),
+  path('firewall/', views.firewall, name='firewall'),
+  path('image/', views.image, name='image'),
+  path('monitoring/', views.monitoring, name='monitoring'),
+
+  path('payment/', views.payment, name='payment'),
+  path('billing/', views.billing, name='billing'),
+  path('resource_record/', views.resource_record, name='resource_record'),
+  # path('/', views., name=''),
+
+
+  path('financial/', views.financial, name='financial'),
+  path('support/', views.support, name='support'),
+  path('affiliate/', views.affiliate, name='affiliate'),
+  path('account/', views.account, name='account'),
 ]
--- home/views.py
+++ home/views.py
@@ -21,3 +21,121 @@ def tables(request):
     'segment': 'tables'
   }
   return render(request, "pages/dynamic-tables.html", context)
+
+
+def instances(request):
+  context = {
+    'segment': 'instances'
+  }
+  return render(request, "pages/instances.html", context)
+
+
+def create_instances(request):
+  context = {
+    'segment': 'create_instances'
+  }
+  return render(request, "pages/create-instances.html", context)
+
+
+
+def network(request):
+  context = {
+    'segment': 'network'
+  }
+  return render(request, "pages/dynamic-tables.html", context)
+
+
+def block_storage(request):
+  context = {
+    'segment': 'block_storage'
+  }
+  return render(request, "pages/dynamic-tables.html", context)
+
+
+
+def snapshot(request):
+  context = {
+    'segment': 'snapshot'
+  }
+  return render(request, "pages/dynamic-tables.html", context)
+
+
+
+def firewall(request):
+  context = {
+    'segment': 'firewall'
+  }
+  return render(request, "pages/dynamic-tables.html", context)
+
+
+
+def image(request):
+  context = {
+    'segment': 'image'
+  }
+  return render(request, "pages/dynamic-tables.html", context)
+
+
+
+def monitoring(request):
+  context = {
+    'segment': 'monitoring'
+  }
+  return render(request, "pages/dynamic-tables.html", context)
+
+
+def payment(request):
+  context = {
+    'segment': 'payment'
+  }
+  return render(request, "pages/dynamic-tables.html", context)
+
+
+
+def resource_record(request):
+  context = {
+    'segment': 'resource_record'
+  }
+  return render(request, "pages/dynamic-tables.html", context)
+
+
+
+def billing(request):
+  context = {
+    'segment': 'billing'
+  }
+  return render(request, "pages/dynamic-tables.html", context)
+
+
+
+
+
+def financial(request):
+  context = {
+    'segment': 'financial'
+  }
+  return render(request, "pages/dynamic-tables.html", context)
+
+
+
+def support(request):
+  context = {
+    'segment': 'support'
+  }
+  return render(request, "pages/dynamic-tables.html", context)
+
+
+
+def affiliate(request):
+  context = {
+    'segment': 'affiliate'
+  }
+  return render(request, "pages/dynamic-tables.html", context)
+
+
+
+def account(request):
+  context = {
+    'segment': 'account'
+  }
+  return render(request, "pages/dynamic-tables.html", context)
