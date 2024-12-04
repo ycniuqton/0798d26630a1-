290526ed94b81ba220c38bc27e776bbfafc63455
Quinh <quinh>
2024-12-04 23:25:55 +0700

    feat: add choice username, email, account balance

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,12 +1,13 @@
 # myapp/context_processors.py
 
 from config import APPConfig, CustomInfo
+from home.models import User
 
 
 def my_constants(request):
-    """
-    A context processor that adds the MY_CONSTANT setting to the context.
-    """
+    all_user = User.objects.filter(is_active=True).all()
+    list_user = [user.username for user in all_user]
+    list_email = [user.email for user in all_user]
     return {
         'APP_ROLE': APPConfig.APP_ROLE,
         "VND_USD_EXCHANGE_RATE": APPConfig.VND_USD_EXCHANGE_RATE,
@@ -15,5 +16,6 @@ def my_constants(request):
         "INVOICE_TERM_CONDITION": CustomInfo.INVOICE_TERM_CONDITION,
         "COMPANY_ADDRESS": CustomInfo.COMPANY_ADDRESS,
         "COMPANY_PHONE": CustomInfo.COMPANY_PHONE,
-
+        "ALL_EMAIL": list_email,
+        "ALL_USERNAME": list_user
     }
